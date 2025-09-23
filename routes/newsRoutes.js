400ab02a65004d4cc9c16b55a849c14972c4c812
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2025-09-23 12:13:04 +0530

    Upload folder miskate solve

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -6,7 +6,7 @@ const News = require("../models/News");
 const router = express.Router();
 
 // Serve uploads publicly (this ensures URLs like https://yourdomain.com/uploads/filename.png work)
-router.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+router.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 const backendUrl = process.env.BACKEND_URL || "https://backendnews-h3lh.onrender.com";
 
