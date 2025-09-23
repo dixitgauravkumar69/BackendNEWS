df61d18db6786dd6b17cf0577478201d414534f3
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2025-09-23 13:40:14 +0530

    Upload folder in render also

--- Server.js
+++ Server.js
@@ -2,18 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 const newsRoutes = require("./routes/newsRoutes");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
+// Ensure uploads folder exists
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Serve uploads folder
+app.use("/uploads", express.static(uploadDir));
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 app.use("/news", newsRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
